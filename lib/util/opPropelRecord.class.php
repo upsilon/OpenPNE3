f894f58a54e8fbc0001edb0dfebf31993aff3592
Kimura Youichi <kim.upsilon@bucyou.net>
2011-08-24 14:54:03 +0900

    PHP5.2以前でNOTICEが出る問題を修正

--- lib/util/opPropelRecord.class.php
+++ lib/util/opPropelRecord.class.php
@@ -4,8 +4,7 @@ class opPropelRecord extends BaseObject implements Zend_Acl_Resource_Interface
 {
   public function getResourceId()
   {
-    $peerClassName = $this::PEER;
-    $tableName = $peerClassName::TABLE_NAME;
+    $tableName = constant(static::PEER.'::TABLE_NAME');
     $identifier = array_values((array)$this->getPrimaryKey());
     $identifier = array_shift($identifier);
 
